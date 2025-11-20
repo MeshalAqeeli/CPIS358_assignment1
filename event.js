a935319d1d13ea512c6f580a7b5d70862df85c54
MeshalAqeeli <meshal.aqily11@gmail.com>
2025-11-20 17:10:56 +0300

    add login validation

--- event.js
+++ event.js
@@ -1,4 +1,16 @@
 function toSignup() 
 {
  window.location.href="signup.html"
+    }
+
+function validateLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username === "" || password === "") {
+        alert("Please enter your username and password");
+        return;
+    } else {
+        alert("Login successful!");
+        return;
+    }
 }
